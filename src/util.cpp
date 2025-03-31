b83fddca7841e65741700a88b9b7d3c8324f9713
traaitt | TRRXITTE <traaitt@icloud.com>
2025-03-31 15:11:59 +0200

    add datadir

--- src/util.cpp
+++ src/util.cpp
@@ -461,7 +461,7 @@ boost::filesystem::path GetDefaultDataDir()
     // Unix: ~/.bitcoin
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "Bitcoin";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "TRRXITTE-BTC";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -471,10 +471,10 @@ boost::filesystem::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/Bitcoin";
+    return pathRet / "Library/Application Support/TRRXITTE-BTC";
 #else
     // Unix
-    return pathRet / ".bitcoin";
+    return pathRet / ".trrxitte-btc";
 #endif
 #endif
 }
