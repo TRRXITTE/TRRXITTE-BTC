8e80fae96ca9d0adfbbccc7a3c8b02fb4bc01a48
traaitt | TRRXITTE <traaitt@icloud.com>
2025-04-01 22:24:22 +0200

    add foundation

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -91,7 +91,7 @@ static void InitMessage(const std::string &message)
  */
 static std::string Translate(const char* psz)
 {
-    return QCoreApplication::translate("bitcoin-core", psz).toStdString();
+    return QCoreApplication::translate("TRRXITTE-BTC", psz).toStdString();
 }
 
 static QString GetLangTerritory()
@@ -497,7 +497,7 @@ void BitcoinApplication::shutdownResult(int retval)
 
 void BitcoinApplication::handleRunawayException(const QString &message)
 {
-    QMessageBox::critical(0, "Runaway exception", BitcoinGUI::tr("A fatal error occurred. Bitcoin can no longer continue safely and will quit.") + QString("\n\n") + message);
+    QMessageBox::critical(0, "Runaway exception", BitcoinGUI::tr("A fatal error occurred. TRRXITTE wallet can no longer continue safely and will quit.") + QString("\n\n") + message);
     ::exit(EXIT_FAILURE);
 }
 
