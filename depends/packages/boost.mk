105c46d8c899a19630161548e04d81f299d09369
traaitt | TRRXITTE <traaitt@icloud.com>
2025-04-03 04:29:51 +0200

    add unix + darwin depends

--- depends/packages/boost.mk
+++ depends/packages/boost.mk
@@ -5,44 +5,50 @@ $(package)_file_name=boost_$(subst .,_,$($(package)_version)).tar.gz
 $(package)_sha256_hash=f55c340aa49763b1925ccf02b2e83f35fdcf634c9d5164a2acb87540173c741d
 
 define $(package)_set_vars
-$(package)_config_opts_release=variant=release
-$(package)_config_opts_debug=variant=debug
-$(package)_config_opts=--layout=tagged --build-type=complete --user-config=user-config.jam
-$(package)_config_opts+=threading=multi link=static -sNO_BZIP2=1 -sNO_ZLIB=1
-$(package)_config_opts_linux=target-os=linux threadapi=pthread runtime-link=shared
-$(package)_config_opts_darwin=target-os=darwin runtime-link=shared
-$(package)_config_opts_mingw32=target-os=windows binary-format=pe runtime-link=static
-$(package)_config_opts_x86_64_mingw32=address-model=64
-$(package)_config_opts_i686_mingw32=address-model=32
-$(package)_config_opts_i686_linux=address-model=32 architecture=x86
-$(package)_config_opts_i686_android=address-model=32
-$(package)_config_opts_aarch64_android=address-model=64
-$(package)_config_opts_x86_64_android=address-model=64
-$(package)_config_opts_armv7a_android=address-model=32
-$(package)_toolset_$(host_os)=gcc
-$(package)_toolset_darwin=clang
-ifneq (,$(findstring clang,$($(package)_cxx)))
-   $(package)_toolset_$(host_os)=clang
-endif
-$(package)_archiver_$(host_os)=$($(package)_ar)
-$(package)_config_libraries=chrono,filesystem,system,thread,test
-$(package)_cxxflags=-std=c++17 -fvisibility=hidden
-$(package)_cxxflags_linux=-fPIC
-$(package)_cxxflags_android=-fPIC
+  $(package)_config_opts_release=variant=release
+  $(package)_config_opts_debug=variant=debug
+  $(package)_config_opts=--layout=tagged --build-type=complete --user-config=user-config.jam
+  $(package)_config_opts+=threading=multi link=static -sNO_BZIP2=1 -sNO_ZLIB=1
+  $(package)_config_opts_linux=target-os=linux threadapi=pthread runtime-link=shared
+  $(package)_config_opts_darwin=target-os=darwin runtime-link=shared
+  $(package)_config_opts_mingw32=target-os=windows binary-format=pe runtime-link=static threadapi=win32
+  $(package)_config_opts_x86_64_mingw32=address-model=64
+  $(package)_config_opts_i686_mingw32=address-model=32
+  $(package)_config_opts_i686_linux=address-model=32 architecture=x86
+  $(package)_config_opts_i686_android=address-model=32
+  $(package)_config_opts_aarch64_android=address-model=64
+  $(package)_config_opts_x86_64_android=address-model=64
+  $(package)_config_opts_armv7a_android=address-model=32
+  # Compilers
+  $(package)_cxx_darwin=/home/traaitt/bitcoin/depends/x86_64-apple-darwin16/native/bin/clang++
+  $(package)_cxx_mingw32=x86_64-w64-mingw32-g++
+  # Toolsets
+  $(package)_toolset_darwin=clang
+  $(package)_toolset_mingw32=gcc
+  $(package)_toolset_linux=gcc
+  $(package)_archiver_darwin=$($(package)_ar)
+  $(package)_archiver_mingw32=$($(package)_ar)
+  $(package)_config_libraries=chrono,filesystem,system,thread,test,program_options
+  $(package)_cxxflags=-std=c++17 -fvisibility=hidden
+  $(package)_cxxflags_linux=-fPIC
+  $(package)_cxxflags_darwin=-fPIC -isysroot /home/traaitt/bitcoin/depends/SDKs/MacOSX11.3.sdk -target x86_64-apple-darwin16
+  $(package)_cxxflags_mingw32=-DWIN32 -D_WIN32 -D__USE_MINGW_ANSI_STDIO=1
 endef
 
 define $(package)_preprocess_cmds
-  echo "using $($(package)_toolset_$(host_os)) : : $($(package)_cxx) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$($(package)_archiver_$(host_os))\" <striper>\"$(host_STRIP)\"  <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam
+  echo "using $($(package)_toolset_$(host_os)) : : $($(package)_cxx_$(host_os)) : <cxxflags>\"$($(package)_cxxflags) $($(package)_cppflags)\" <linkflags>\"$($(package)_ldflags)\" <archiver>\"$($(package)_archiver_$(host_os))\" <striper>\"$(host_STRIP)\" <ranlib>\"$(host_RANLIB)\" <rc>\"$(host_WINDRES)\" : ;" > user-config.jam
 endef
 
 define $(package)_config_cmds
-  ./bootstrap.sh --without-icu --with-libraries=$($(package)_config_libraries) --with-toolset=$($(package)_toolset_$(host_os))
+  ./bootstrap.sh --without-icu --with-libraries=$($(package)_config_libraries) --with-toolset=$($(package)_toolset_$(host_os)) CXX=$($(package)_cxx_$(host_os))
 endef
 
 define $(package)_build_cmds
-  ./b2 -d2 -j2 -d1 --prefix=$($(package)_staging_prefix_dir) $($(package)_config_opts) toolset=$($(package)_toolset_$(host_os)) stage
+  NPROC=$$(nproc 2>/dev/null || echo 4); \
+  ./b2 -d2 --prefix=$($(package)_staging_prefix_dir) $($(package)_config_opts) toolset=$($(package)_toolset_$(host_os)) stage
 endef
 
 define $(package)_stage_cmds
-  ./b2 -d0 -j4 --prefix=$($(package)_staging_prefix_dir) $($(package)_config_opts) toolset=$($(package)_toolset_$(host_os)) install
+  NPROC=$$(nproc 2>/dev/null || echo 4); \
+  ./b2 -d0--prefix=$($(package)_staging_prefix_dir) $($(package)_config_opts) toolset=$($(package)_toolset_$(host_os)) install
 endef
--- depends/packages/native_clang.mk
+++ depends/packages/native_clang.mk
@@ -1,14 +1,15 @@
+
 package=native_clang
-$(package)_version=10.0.1
+$(package)_version=16.0.0
 $(package)_download_path=https://github.com/llvm/llvm-project/releases/download/llvmorg-$($(package)_version)
 ifneq (,$(findstring aarch64,$(BUILD)))
 $(package)_download_file=clang+llvm-$($(package)_version)-aarch64-linux-gnu.tar.xz
 $(package)_file_name=clang+llvm-$($(package)_version)-aarch64-linux-gnu.tar.xz
-$(package)_sha256_hash=90dc69a4758ca15cd0ffa45d07fbf5bf4309d47d2c7745a9f0735ecffde9c31f
+$(package)_sha256_hash=684143b9d532f5ebfd9433b08e5d03e8a9b6569f05c9b0ec39f2b265137b8b98
 else
-$(package)_download_file=clang+llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-$(package)_file_name=clang+llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-$(package)_sha256_hash=48b83ef827ac2c213d5b64f5ad7ed082c8bcb712b46644e0dc5045c6f462c231
+$(package)_download_file=clang+llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+$(package)_file_name=clang+llvm-$($(package)_version)-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+$(package)_sha256_hash=2b8a69798e8dddeb57a186ecac217a35ea45607cb2b3cf30014431cff4340ad1
 endif
 
 define $(package)_preprocess_cmds
@@ -16,15 +17,15 @@ define $(package)_preprocess_cmds
 endef
 
 define $(package)_stage_cmds
-  mkdir -p $($(package)_staging_prefix_dir)/lib/clang/$($(package)_version)/include && \
+  mkdir -p $($(package)_staging_prefix_dir)/lib/clang/16/include && \
   mkdir -p $($(package)_staging_prefix_dir)/bin && \
   mkdir -p $($(package)_staging_prefix_dir)/include && \
-  cp bin/clang $($(package)_staging_prefix_dir)/bin/ && \
-  cp -P bin/clang++ $($(package)_staging_prefix_dir)/bin/ && \
-  cp bin/dsymutil $($(package)_staging_prefix_dir)/bin/$(host)-dsymutil && \
-  cp bin/llvm-config $($(package)_staging_prefix_dir)/bin/ && \
-  cp lib/libLTO.so $($(package)_staging_prefix_dir)/lib/ && \
-  cp -rf lib/clang/$($(package)_version)/include/* $($(package)_staging_prefix_dir)/lib/clang/$($(package)_version)/include/
+  cp $($(package)_extract_dir)/bin/clang $($(package)_staging_prefix_dir)/bin/ && \
+  cp -P $($(package)_extract_dir)/bin/clang++ $($(package)_staging_prefix_dir)/bin/ && \
+  cp $($(package)_extract_dir)/bin/dsymutil $($(package)_staging_prefix_dir)/bin/$(host)-dsymutil && \
+  cp $($(package)_extract_dir)/bin/llvm-config $($(package)_staging_prefix_dir)/bin/ && \
+  cp $($(package)_extract_dir)/lib/libLTO.so $($(package)_staging_prefix_dir)/lib/ && \
+  cp -rf $($(package)_extract_dir)/lib/clang/16/include/* $($(package)_staging_prefix_dir)/lib/clang/16/include/
 endef
 
 define $(package)_postprocess_cmds
